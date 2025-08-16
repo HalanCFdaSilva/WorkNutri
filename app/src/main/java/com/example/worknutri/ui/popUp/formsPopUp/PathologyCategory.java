a7f4e98decdba44dd15c5c86cd5892e927a89209
HalanCFdaSilva <halansilva@rocketmail.com>
2025-08-15 21:13:36 -0300

    arrumado erros na classe

--- app/src/main/java/com/example/worknutri/ui/popUp/formsPopUp/PathologyCategory.java
+++ app/src/main/java/com/example/worknutri/ui/popUp/formsPopUp/PathologyCategory.java
@@ -1,18 +1,20 @@
 package com.example.worknutri.ui.popUp.formsPopUp;
 
+import com.example.worknutri.R;
+
 public enum PathologyCategory {
-    ACTUAL_PATHOLOGY,
-    URINE,
-    STOOL,
-    SLUMBER,
-    MEDICATION,
-    SUPPLEMENT,
-    ETHYLIC,
-    SMOKER,
-    ALLERGY,
-    WATER,
-    SUGAR,
-    ACTIVITY;
+    ACTUAL_PATHOLOGY ("Patologia Atual:", R.string.actual_pathology_hint),
+    URINE("Urina:", R.string.urine_hint),
+    STOOL("Fezes:", R.string.stool_hint),
+    SLUMBER("Sono:", R.string.slumber_hint),
+    MEDICATION("Medicação:", R.string.medication_hint),
+    SUPPLEMENT("Suplementação:", R.string.supplement_hint),
+    ETHYLIC("Etilico:", R.string.ethylic_hint),
+    SMOKER("Fumante:", R.string.smoker_hint),
+    ALLERGY("Alergia:", R.string.allergy_hint),
+    WATER("Água:", R.string.water_hint),
+    SUGAR("Açúcar:", R.string.sugar_hint),
+    ACTIVITY("Atividade Fisica:", R.string.activity_hint),;
 
     private final String name;
     private final int hint;

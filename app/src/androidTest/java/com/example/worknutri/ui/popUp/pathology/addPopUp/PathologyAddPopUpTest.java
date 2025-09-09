93baaf3f0edced2fac049b14355ff13be5e76673
HalanCFdaSilva <halansilva@rocketmail.com>
2025-09-09 20:21:43 -0300

    Criado testes para a classe PathologyAddPopUp

---
+++ app/src/androidTest/java/com/example/worknutri/ui/popUp/pathology/addPopUp/PathologyAddPopUpTest.java
@@ -0,0 +1,126 @@
+package com.example.worknutri.ui.popUp.pathology.addPopUp;
+
+import android.content.Context;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.MultiAutoCompleteTextView;
+import android.widget.Spinner;
+import android.widget.SpinnerAdapter;
+import android.widget.TextView;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.example.worknutri.R;
+import com.example.worknutri.ui.popUp.pathology.PathologyField;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+@RunWith(AndroidJUnit4.class)
+public class PathologyAddPopUpTest {
+
+    private PathologyAddPopUp pathologyAddPopUp;
+    private Context context;
+
+
+    @Before
+    public void setUp() {
+
+        context = InstrumentationRegistry.getInstrumentation().getTargetContext();
+        context = new ContextThemeWrapper(context, R.style.Theme_NutriCoop);
+        pathologyAddPopUp = new PathologyAddPopUp(context, List.of(PathologyField.values()));
+    }
+
+    @Test
+    public void verifyIfNewPathologyAddPopUpInflateTheLayout(){
+        pathologyAddPopUp = new PathologyAddPopUp(context,List.of(PathologyField.values()));
+        ViewGroup viewGroup = pathologyAddPopUp.getViewGroup();
+        Assert.assertNotNull(viewGroup);
+    }
+
+    @Test
+    public void verifyIfViewGroupIsPopupBaseLayout(){
+        pathologyAddPopUp = new PathologyAddPopUp(context,List.of(PathologyField.values()));
+        ViewGroup viewGroup = pathologyAddPopUp.getViewGroup();
+        Assert.assertNotNull(viewGroup);
+        Assert.assertEquals(R.id.popup_base_layout,viewGroup.getId());
+    }
+
+    @Test
+    public void verifyIfInflatePopUpPatologiaAddCorrectly(){
+        pathologyAddPopUp = new PathologyAddPopUp(context,List.of(PathologyField.values()));
+        ViewGroup viewGroup = pathologyAddPopUp.getViewGroup();
+        Assert.assertNotNull(viewGroup);
+        LinearLayout layout = viewGroup.findViewById(R.id.popup_base_layout_layout_intern);
+        Assert.assertNotNull(layout);
+        Assert.assertEquals(1,layout.getChildCount());
+        ViewGroup viewWereVerify = (ViewGroup) layout.getChildAt(0);
+        Assert.assertEquals(R.id.popup_patologia_add,viewWereVerify.getId());
+    }
+
+    @Test
+    public void verifyIfPopUpPatologiaAddLayoutCorrectly(){
+        pathologyAddPopUp = new PathologyAddPopUp(context,List.of(PathologyField.values()));
+        ViewGroup viewGroup = pathologyAddPopUp.getViewGroup();
+        Assert.assertNotNull(viewGroup);
+        ViewGroup viewToVerify = viewGroup.findViewById(R.id.popup_patologia_add);
+        Assert.assertNotNull(viewToVerify);
+        Assert.assertEquals(3,viewToVerify.getChildCount());
+        View childToVerify = viewToVerify.getChildAt(0);
+        Assert.assertEquals(R.id.popup_patologia_add_spinner,childToVerify.getId());
+        Assert.assertTrue(childToVerify instanceof Spinner);
+
+        childToVerify = viewToVerify.getChildAt(1);
+        Assert.assertEquals(R.id.popup_patologia_add_multiAutoComplete,childToVerify.getId());
+        Assert.assertTrue(childToVerify instanceof MultiAutoCompleteTextView);
+
+        childToVerify = viewToVerify.getChildAt(2);
+        Assert.assertEquals(R.id.popup_patologia_add_button,childToVerify.getId());
+        Assert.assertTrue(childToVerify instanceof Button);
+    }
+
+    @Test
+    public void verifyIfTitleIsInsertedCorrectly(){
+        pathologyAddPopUp = new PathologyAddPopUp(context,List.of(PathologyField.values()));
+        ViewGroup viewGroup = pathologyAddPopUp.getViewGroup();
+        Assert.assertNotNull(viewGroup);
+        View title = viewGroup.findViewById(R.id.popup_base_layout_title_textview_header);
+        Assert.assertNotNull(title);
+        Assert.assertEquals(View.VISIBLE,title.getVisibility());
+        Assert.assertTrue(title instanceof TextView);
+        TextView titleTextView = (TextView) title;
+        Assert.assertEquals(context.getString(R.string.patologia_title),titleTextView.getText().toString());
+    }
+
+    @Test
+    public void verifyIfConfigureLayoutInsertDataInSpinnerCorrectly(){
+        List<PathologyField> listOfPathologiesToInsertInSpinner = List.of(PathologyField.values());
+        pathologyAddPopUp = new PathologyAddPopUp(context, listOfPathologiesToInsertInSpinner);
+        ViewGroup viewGroup = pathologyAddPopUp.getViewGroup();
+        Assert.assertNotNull(viewGroup);
+        pathologyAddPopUp.configurePopUp(new LinearLayout(context),null);
+        ViewGroup viewToVerify = viewGroup.findViewById(R.id.popup_patologia_add);
+        Assert.assertNotNull(viewToVerify);
+        Assert.assertEquals(3,viewToVerify.getChildCount());
+        View childToVerify = viewToVerify.getChildAt(0);
+        Assert.assertEquals(R.id.popup_patologia_add_spinner,childToVerify.getId());
+        Assert.assertTrue(childToVerify instanceof Spinner);
+        SpinnerAdapter spinnerAdapter = ((Spinner) childToVerify).getAdapter();
+        for (int i = 0; i < spinnerAdapter.getCount(); i++) {
+            String item = (String) spinnerAdapter.getItem(i);
+            Assert.assertTrue(listOfPathologiesToInsertInSpinner.stream()
+                    .anyMatch(pathologyField -> pathologyField.getUpperName().equals(item)));
+        }
+    }
+
+
+
+}

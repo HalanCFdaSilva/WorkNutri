07c9d3c976f6189a2b5e1e7d381476289232f234
HalanCFdaSilva <halansilva@rocketmail.com>
2026-02-18 19:51:27 -0300

    refatorado a classe a passando para o inglês

--- app/src/test/java/com/example/worknutri/calcular/AnthropometricCalculatorTest.java
+++ app/src/test/java/com/example/worknutri/calcular/AnthropometricCalculatorTest.java
@@ -9,165 +9,165 @@ import java.time.format.DateTimeFormatter;
 
 
 public class AnthropometricCalculatorTest {
-    AntropometricCalculator calculador;
+    AntropometricCalculator calculator;
 
     @Before
-    public void generateCalculadorAntropometrico(){
-        calculador = new AntropometricCalculator(70,1.8);
+    public void setUp(){
+        calculator = new AntropometricCalculator(70,1.8);
     }
 
     @Test
-    public void calculaImcCorretamente(){
-        AntropometricCalculator calculador = new AntropometricCalculator(67.5,1.5);
-        double imc = Double.parseDouble(calculador.generateImc());
+    public void calculateImcCorrectly(){
+        AntropometricCalculator calculator = new AntropometricCalculator(67.5,1.5);
+        double imc = Double.parseDouble(calculator.generateImc());
         Assert.assertEquals(30.0,imc,0.5);
     }
 
     @Test
-    public void calculaTaxaMetabolicaBasalCorretamenteParaHomens(){
+    public void calculateMetabolicRateCorrectlyInMan(){
         double tmbExpected = (10 * 70) + (6.25 * 180) - (5 * 25) + 5;
-        double tmb = Double.parseDouble(calculador.generateTMB('M',25));
+        double tmb = Double.parseDouble(calculator.generateTMB('M',25));
         Assert.assertEquals(tmbExpected,tmb,0);
 
     }
 
     @Test
-    public void calculaTaxaMetabolicaBasalCorretamenteParaMulheres(){
+    public void calculateMetabolicRateCorrectlyInWoman(){
         double tmbExpected = (10 * 70) + (6.25 * 180) - (5 * 25) - 161;
-        double tmb = Double.parseDouble(calculador.generateTMB('F',25));
+        double tmb = Double.parseDouble(calculator.generateTMB('F',25));
         Assert.assertEquals(tmbExpected,tmb,0);
 
     }
 
     @Test
-    public void calculaGastoEnergeticoCorretamenteParaHomensSedentarios(){
-        double tmb = Double.parseDouble(calculador.generateTMB('M',25));
+    public void calculateEnergyExpendCorrectlyInMenSedentary(){
+        double tmb = Double.parseDouble(calculator.generateTMB('M',25));
         double getExpected = tmb * 1.2;
-        double getReceived = Double.parseDouble(calculador.generateGET(tmb,0));
+        double getReceived = Double.parseDouble(calculator.generateGET(tmb,0));
         Assert.assertEquals(getExpected,getReceived,0);
 
     }
 
     @Test
-    public void calculaGastoEnergeticoCorretamenteParaHomensLevementeAtivos(){
-        double tmb = Double.parseDouble(calculador.generateTMB('M',25));
+    public void calculateEnergyExpendCorrectlyInMenLightlyActive(){
+        double tmb = Double.parseDouble(calculator.generateTMB('M',25));
         double getExpected = tmb * 1.375;
-        double getReceived = Double.parseDouble(calculador.generateGET(tmb,1));
+        double getReceived = Double.parseDouble(calculator.generateGET(tmb,1));
         Assert.assertEquals(getExpected,getReceived,0);
 
     }
 
     @Test
-    public void calculaGastoEnergeticoCorretamenteParaHomensModeradamenteAtivos(){
-        double tmb = Double.parseDouble(calculador.generateTMB('M',25));
+    public void calculateEnergyExpendCorrectlyInInMenModeratelyActive(){
+        double tmb = Double.parseDouble(calculator.generateTMB('M',25));
         double getExpected = tmb * 1.55;
-        double getReceived = Double.parseDouble(calculador.generateGET(tmb,2));
+        double getReceived = Double.parseDouble(calculator.generateGET(tmb,2));
         Assert.assertEquals(getExpected,getReceived,0);
 
     }
 
     @Test
-    public void calculaGastoEnergeticoCorretamenteParaHomensAtividadeIntensa(){
-        double tmb = Double.parseDouble(calculador.generateTMB('M',25));
+    public void calculateEnergyExpendCorrectlyInInMenIntenseActivity(){
+        double tmb = Double.parseDouble(calculator.generateTMB('M',25));
         double getExpected = tmb * 1.9;
-        double getReceived = Double.parseDouble(calculador.generateGET(tmb,3));
+        double getReceived = Double.parseDouble(calculator.generateGET(tmb,3));
         Assert.assertEquals(getExpected,getReceived,0);
 
     }
 
     @Test
-    public void calculaGastoEnergeticoCorretamenteParaMulheresSedentarias(){
-        double femaletmb = Double.parseDouble(calculador.generateTMB('F',25));
-        double getExpected = femaletmb * 1.2;
-        double getReceived = Double.parseDouble(calculador.generateGET(femaletmb,0));
+    public void calculateEnergyExpendCorrectlyInInWomenSedentary(){
+        double femaleTmb = Double.parseDouble(calculator.generateTMB('F',25));
+        double getExpected = femaleTmb * 1.2;
+        double getReceived = Double.parseDouble(calculator.generateGET(femaleTmb,0));
         Assert.assertEquals(getExpected,getReceived,0);
 
     }
 
     @Test
-    public void calculaGastoEnergeticoCorretamenteParaMulheresLevementeAtivas(){
-        double femaletmb = Double.parseDouble(calculador.generateTMB('F',25));
-        double getExpected = femaletmb * 1.375;
-        double getReceived = Double.parseDouble(calculador.generateGET(femaletmb,1));
+    public void calculateEnergyExpendCorrectlyInInWomenLightlyActive(){
+        double femaleTmb = Double.parseDouble(calculator.generateTMB('F',25));
+        double getExpected = femaleTmb * 1.375;
+        double getReceived = Double.parseDouble(calculator.generateGET(femaleTmb,1));
         Assert.assertEquals(getExpected,getReceived,0);
 
 
     }
 
     @Test
-    public void calculaGastoEnergeticoCorretamenteParaMulheresModeradamenteAtivas(){
-        double femaletmb = Double.parseDouble(calculador.generateTMB('F',25));
-        double getExpected = femaletmb * 1.55;
-        double getReceived = Double.parseDouble(calculador.generateGET(femaletmb,2));
+    public void calculateEnergyExpendCorrectlyInInWomenModeratelyActive(){
+        double femaleTmb = Double.parseDouble(calculator.generateTMB('F',25));
+        double getExpected = femaleTmb * 1.55;
+        double getReceived = Double.parseDouble(calculator.generateGET(femaleTmb,2));
         Assert.assertEquals(getExpected,getReceived,0);
 
     }
 
     @Test
-    public void calculaGastoEnergeticoCorretamenteParaMulheresAtividadeIntensa(){
-        double femaletmb = Double.parseDouble(calculador.generateTMB('F',25));
-        double getExpected = femaletmb * 1.9;
-        double getReceived = Double.parseDouble(calculador.generateGET(femaletmb,3));
+    public void calculateEnergyExpendCorrectlyInInWomenIntenseActivity(){
+        double femaleTmb = Double.parseDouble(calculator.generateTMB('F',25));
+        double getExpected = femaleTmb * 1.9;
+        double getReceived = Double.parseDouble(calculator.generateGET(femaleTmb,3));
         Assert.assertEquals(getExpected,getReceived,0);
 
     }
 
     @Test
-    public void calculaPesoIdealCorretamente(){
+    public void calculateIdealWeightCorrectly(){
         double idealWeight = 1.8 * 1.8 * 22;
         Assert.assertEquals(idealWeight, AntropometricCalculator.generatePesoIdeal(1.8),0.001);
 
     }
 
     @Test
-    public void calculaRegraDeBolsoCorretamenteParaPesoIgualIdeal(){
-        double pesoIdeal = AntropometricCalculator.generatePesoIdeal(1.78);
-        double bolsoCalculado = Double.parseDouble(calculador.generateBolso(pesoIdeal));
-        Assert.assertEquals(0,bolsoCalculado,0.001);
+    public void calculateThumbCorrectlyToWeightEqualIdealWeight(){
+        double idealWeight = AntropometricCalculator.generatePesoIdeal(1.78);
+        double thumbCalculate = Double.parseDouble(calculator.generateBolso(idealWeight));
+        Assert.assertEquals(0,thumbCalculate,0.001);
     }
 
     @Test
-    public void calculaRegraDeBolsoCorretamenteParaPesoMaiorIdeal(){
-        double pesoIdeal = AntropometricCalculator.generatePesoIdeal(1.78);
-        calculador.setWeight(80);
-        double bolsoCalculado = Double.parseDouble(calculador.generateBolso(pesoIdeal));
-        Assert.assertEquals(-1600,bolsoCalculado,0.001);
+    public void calculateThumbCorrectlyToWeightGreaterThanIdealWeight(){
+        double idealWeight = AntropometricCalculator.generatePesoIdeal(1.78);
+        calculator.setWeight(80);
+        double thumbCalculate = Double.parseDouble(calculator.generateBolso(idealWeight));
+        Assert.assertEquals(-1600,thumbCalculate,0.001);
     }
 
     @Test
-    public void calculaRegraDeBolsoCorretamenteParaPesoMenorIdeal(){
-        double pesoIdeal = AntropometricCalculator.generatePesoIdeal(1.78);
-        calculador.setWeight(60);
-        double bolsoCalculado = Double.parseDouble(calculador.generateBolso(pesoIdeal));
-        Assert.assertEquals(1800,bolsoCalculado,0.001);
+    public void calculateThumbCorrectlyToWeightLessThanIdealWeight(){
+        double idealWeight = AntropometricCalculator.generatePesoIdeal(1.78);
+        calculator.setWeight(60);
+        double thumbCalculate = Double.parseDouble(calculator.generateBolso(idealWeight));
+        Assert.assertEquals(1800,thumbCalculate,0.001);
     }
 
     @Test
-    public void fazerOCalculoDeVentaCorretamenteParaUmKg(){
-        double get = getEnergicCoast();
+    public void calculateVentaCorrectlyToOneKg(){
+        double get = getEnergyExpend();
         double ventaExpected = get - (double) 770/3;
-        double venta = Double.parseDouble(calculador.generateVenta(get,0));
+        double venta = Double.parseDouble(calculator.generateVenta(get,0));
         Assert.assertEquals(ventaExpected,venta,0.001);
     }
 
     @Test
     public void calculateVentaCorrectlyForMoreThanOneKg(){
-        double get = getEnergicCoast();
+        double get = getEnergyExpend();
         double valorKg = (double)770 / 3;
         for (int i = 0; i <=3; i++) {
             double ventaExpected = get - valorKg  - (valorKg/2)*i;
-            double venta = Double.parseDouble(calculador.generateVenta(get,i));
+            double venta = Double.parseDouble(calculator.generateVenta(get,i));
             Assert.assertEquals(ventaExpected,venta,0.001);
         }
     }
 
-    private double getEnergicCoast() {
-        double tmb = Double.parseDouble(calculador.generateTMB('F',25));
-        return Double.parseDouble(calculador.generateGET(tmb,0));
+    private double getEnergyExpend() {
+        double tmb = Double.parseDouble(calculator.generateTMB('F',25));
+        return Double.parseDouble(calculator.generateGET(tmb,0));
     }
 
     @Test
-    public void calcuateYearFromDateCorrectly(){
+    public void calculateYearFromDateCorrectly(){
         int yearExpected = LocalDate.now().getYear() - 1998;
         LocalDate now = LocalDate.now().withYear(1998);
         String born = now.minusDays(1).format(DateTimeFormatter.ofPattern("dd/MM/yyyy"));

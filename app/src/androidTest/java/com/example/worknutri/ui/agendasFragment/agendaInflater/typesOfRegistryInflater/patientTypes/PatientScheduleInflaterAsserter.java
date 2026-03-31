dca4c3d3b9ddafde9fa47dd7bdb1a04127568656
HalanCFdaSilva <halansilva@rocketmail.com>
2026-03-31 11:53:01 -0300

    criado testes para as classes que fazem a categorização dos registros na agenda do paciente

---
+++ app/src/androidTest/java/com/example/worknutri/ui/agendasFragment/agendaInflater/typesOfRegistryInflater/patientTypes/PatientScheduleInflaterAsserter.java
@@ -0,0 +1,71 @@
+package com.example.worknutri.ui.agendasFragment.agendaInflater.typesOfRegistryInflater.patientTypes;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.worknutri.R;
+import com.example.worknutri.sqlLite.domain.paciente.Antropometria;
+import com.example.worknutri.sqlLite.domain.paciente.Paciente;
+import com.example.worknutri.support.TestEntityFactory;
+
+import java.util.Arrays;
+import java.util.List;
+
+public abstract class PatientScheduleInflaterAsserter {
+
+    public static void assertLayout(ViewGroup layout, String[] categoriesNamesExpected,
+                                    List<List<Paciente>> patientsExpected) {
+        for (int i = 0; i < layout.getChildCount(); i++) {
+            ViewGroup category = (ViewGroup) layout.getChildAt(i);
+            assertNotNull(category);
+            assertCategory(category, categoriesNamesExpected[i]);
+            assertCards(category, patientsExpected.get(i));
+        }
+    }
+
+    private static void assertCategory(ViewGroup category, String categoryName) {
+        assertNotNull(category);
+        TextView title = category.findViewById(R.id.registry_letter_fragment_agenda_textview);
+        assertEquals(categoryName, title.getText().toString());
+
+    }
+
+    private static void assertCards(ViewGroup category, List<Paciente> patientsExpectsInOrder) {
+        ViewGroup layout = category.findViewById(R.id.registry_letter_fragment_linear_layout);
+        assertNotNull(layout);
+        assertEquals(patientsExpectsInOrder.size(), layout.getChildCount());
+        for(int i = 0; i < layout.getChildCount(); i++) {
+            Paciente patientExpected = patientsExpectsInOrder.get(i);
+            ViewGroup card = (ViewGroup) layout.getChildAt(i);
+            assertEquals(R.id.schedule_card_fragment_patient, card.getChildAt(0).getId());
+            TextView textViewName = card.findViewById(R.id.registry_card_fragment_patient_textview);
+            assertEquals(patientExpected.getNomePaciente(), textViewName.getText().toString());
+
+        }
+    }
+
+    public static List<Paciente> generatePatientToTest(){
+        Paciente p1 = TestEntityFactory.generatePatient();
+        p1.setId(1); p1.setNomePaciente("Paciente 1");
+
+        Paciente p2 = TestEntityFactory.generatePatient();
+        p2.setId(2); p2.setNomePaciente("Paciente 2");
+
+        Paciente p3 = TestEntityFactory.generatePatient();
+        p3.setId(3); p3.setNomePaciente("Paciente 3");
+        return Arrays.asList(p1, p2, p3);
+    }
+
+    public static List<Antropometria> generateAnthropometryToTest(List<Paciente> patients) {
+        Antropometria a1 = TestEntityFactory.generateAnthropometry("15/08/1990", 1.70, 60);
+        a1.setIdPaciente((int) patients.get(0).getId());
+        Antropometria a2 = TestEntityFactory.generateAnthropometry("15/08/1990", 1.70, 80);
+        a2.setIdPaciente((int) patients.get(1).getId());
+        Antropometria a3 = TestEntityFactory.generateAnthropometry("15/08/1990", 1.70, 90);
+        a3.setIdPaciente((int) patients.get(2).getId());
+        return Arrays.asList(a1, a2, a3);
+    }
+}
---
+++ app/src/androidTest/java/com/example/worknutri/ui/agendasFragment/agendaInflater/typesOfRegistryInflater/patientTypes/WeightAntropometryRegistryInflaterTest.java
@@ -0,0 +1,76 @@
+package com.example.worknutri.ui.agendasFragment.agendaInflater.typesOfRegistryInflater.patientTypes;
+
+import android.content.Context;
+import android.widget.LinearLayout;
+
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import com.example.worknutri.sqlLite.domain.paciente.Antropometria;
+import com.example.worknutri.sqlLite.domain.paciente.Paciente;
+import com.example.worknutri.support.TestUtil;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(AndroidJUnit4.class)
+public class WeightAntropometryRegistryInflaterTest {
+
+    private final Context context = TestUtil.getThemedContext();
+
+    @Test
+    public void generateAgendaCreatesCategoriesAndAddsCards() {
+        List<Paciente> patientList = PatientScheduleInflaterAsserter.generatePatientToTest();
+
+        List<Antropometria> antropometriaList = PatientScheduleInflaterAsserter.generateAnthropometryToTest(patientList);
+
+
+        String[] categoriesNamesExpected = new String[] {
+                "60,00 - 69,99 KG",
+                "80,00 - 89,99 KG",
+                "90,00 - 99,99 KG"
+        };
+
+        List<List<Paciente>> patientsExpectedInCards = List.of(
+                List.of(patientList.get(0)),
+                List.of(patientList.get(1)),
+                List.of(patientList.get(2))
+        );
+
+        LinearLayout parent = new LinearLayout(context);
+        WeightAntropometryRegistryInflater inflater = new WeightAntropometryRegistryInflater(context, antropometriaList);
+        inflater.generateAgenda(parent, patientList);
+
+        assertEquals(3, parent.getChildCount());
+        PatientScheduleInflaterAsserter.assertLayout(parent, categoriesNamesExpected, patientsExpectedInCards);
+    }
+
+    @Test
+    public void ifMoreThenOneRegistryHasSameIntegerPartTheyAreAddedInTheSameCategory() {
+        List<Paciente> patientList = PatientScheduleInflaterAsserter.generatePatientToTest();
+
+        List<Antropometria> antropometriaList = PatientScheduleInflaterAsserter.generateAnthropometryToTest(patientList);
+        antropometriaList.get(0).setPeso("60.0");
+        antropometriaList.get(1).setPeso("65.38");
+        antropometriaList.get(2).setPeso("69.99");
+
+
+
+        String[] categoriesNamesExpected = new String[] {
+                "60,00 - 69,99 KG"
+        };
+
+        List<List<Paciente>> patientsExpectedInCards = List.of( patientList );
+
+        LinearLayout parent = new LinearLayout(context);
+        WeightAntropometryRegistryInflater inflater = new WeightAntropometryRegistryInflater(context, antropometriaList);
+        inflater.generateAgenda(parent,  patientList );
+
+        assertEquals(1, parent.getChildCount());
+        PatientScheduleInflaterAsserter.assertLayout(parent, categoriesNamesExpected, patientsExpectedInCards);
+    }
+}
+
